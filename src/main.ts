4b83f974b7a38d2cb303af552419f3d3c78dc98f
DatoIsko <davitiskandaryan12@gmail.com>
2019-12-09 01:52:00 +0400

    Added origins

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,13 @@ const port = process.env.PORT || 8000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:4200', // angular
+      'http://localhost:3000', // react
+      'http://localhost:8081', // react-native
+    ]
+  });
   await app.listen(port);
   Logger.log(`Server running on http://localhost:${port}`, 'Bootstrap');
 }
